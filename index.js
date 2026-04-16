d91b350d2a214cf8b060745c6f466acdf919e523
juniorAGU <perryfresh25@gmail.com>
2026-04-15 21:07:20 -0700

    Recent Updated todo-card

--- index.js
+++ index.js
@@ -1,22 +1,113 @@
 let checkit = document.getElementById("checkbox");
-let status1 = document.querySelector(".status");
-let label1 = document.querySelector("label");
+let label1 = document.querySelector(".label");
+let overLay = document.querySelector(".overlay");
+let form = document.querySelector(".form")
+
+
+// text contents
+let titleh2 = document.querySelector(".h2");
+let descp = document.querySelector(".dp");
+let statuscl = document.querySelector(".statuscl");
+let date = document.querySelector(".date");
+let totages = document.querySelector(".tags")
+let slstatsu = document.querySelector("#statush");
+
+
+
+let title = document.querySelector("#title");
+let description = document.querySelector("#description");
+let status2 = document.querySelector("#status");
+let priority = document.querySelector("#priority");
+let duedate = document.querySelector("#due-date");
+let tags = document.querySelector("#tags");
+let li = document.querySelector(".li")
+
+
+
+
+let obj = {
+    obgtitle: "",
+    obgdescription: "",
+    obgstatus: "",
+    obgpriority: "",
+    obgduedate: "",
+    obgtags: "",
+
+}
+
+
+function HandleSubmit(e){
+    e.preventDefault();
+    e.stopPropagation()
+
+
+    let pagetitle = title.value.trim();
+    let pagedescription = description.value.trim();
+    let pagestatus = status2.value;
+    let pagepriority = priority.value;
+    let pageduedate = duedate.value;
+    let pagetages = tags.value;
+    console.log(pagetitle, pagedescription, pagestatus, pagepriority, pageduedate, pagetages)
+    
+
+    if(pagetitle === "" || pagedescription === "" || pagestatus === "" || pagepriority === "" || pageduedate === "" || pagetages === ""){
+        alert("please put all your details properly")
+        return false
+    }else{
+        obj.obgtitle = pagetitle;
+        obj.obgdescription = pagedescription;
+        obj.obgstatus = pagestatus;
+        obj.obgpriority = pagepriority;
+        obj.obgduedate = pageduedate;
+        obj.obgtags = pagetages;
+
+
+        titleh2.textContent = obj.obgtitle;
+        descp.textContent = obj.obgdescription;
+        statuscl.textContent = obj.obgpriority
+        date.textContent = obj.obgduedate;
+        totages.textContent = obj.obgtags;
+        
+    localStorage.setItem("obj",JSON.stringify(obj))
+    alert("saved!!!")
+    overLay.style.display = "none";
+    }
+
+    title.value = "";
+    description.value = "";
+    status2.value = "";
+    priority.value = "";
+    duedate.value = "";
+    tags.value = "";
+        
+}
+console.log(obj);
+form.addEventListener("submit", HandleSubmit);
+let obj1 = JSON.parse(localStorage.getItem("obj"));
+console.log(obj1);
+
+
+
+
+
+
 
 
 checkit.addEventListener("change", () => {
     if (checkit.checked) {
-        status1.textContent = "Completed";
-        label1.textContent = "Completed Task";
-        label1.style.textDecoration = "line-through";
-        status1.style.backgroundColor = "#bef0d3";
-        status1.style.color = "#0a7016";
+        label1.textContent = "Completed"
+        slstatsu.innerHTML = `<option value="done">Done
+        </option>`
+
+        
+        
     }else{
-        status1.textContent = "pending";
-        status1.style.backgroundColor = "#f4f4f4";
-    }
+            }
 })
-const dueDate = new Date("2026-04-16T23:59:00Z");
-function SetDueDate() {
+
+
+const dueDate = obj1 ? new Date(obj1.obgduedate) : null;
+function SetDueDate() {l
     let timerem = document.querySelector('[data-testid="test-todo-time-remaining"]')
     if(!timerem){
         return
@@ -45,25 +136,26 @@ SetDueDate()
 
 setInterval(SetDueDate,1)
 
-function Display() {
-   let me = prompt("Why are you trying to edit me!!!! have you completed me??");
-   if(me.toLowerCase() === "yes"){
-    alert("i dont want to be edited leave me alone!!!")
-   }else if(me.toLowerCase() === "no"){
-    alert("so why the fucking hell did you click that edit button respect your self!! ")
-   }else{
-    alert("if get you  that place eehhh!!! ")
-   }
-    
+function EditForm() {
+    overLay.style.display = "flex";
 }
 
-function Getdisplay() {
-    let me = prompt(" come this guy are you sure that you have completed me!!!!");
+function Canclemodal() {
+    let me = prompt(" Are you sure, you don't want to save changes ???");
     if(me.toLowerCase() === "yes"){
-        alert("Bad guy!! i greet you boss")
+        alert("saved!!!")
     }else if(me.toLowerCase() === "no"){
-        alert("Are you fucking with me right now !????")
+        overLay.style.display = "none";
     }else{
-        alert("i no wan talk too much ooo, better go back to were you are comming from")
+        overLay.style.display = "none";
     }
 }
+
+
+
+// titleh2.textContent = obj.obgtitle;
+// descp.textContent = obj.obgdescription;
+// statuscl.textContent = obj.obgstatus
+// date.textContent = obj.obgduedate;
+// totages.textContent = obj.obgtags
+
